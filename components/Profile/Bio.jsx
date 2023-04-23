d573dbb99468650635e97f8e7f7a02e5fbbd8abe
ARYAN SINGLA <72137517+aryansingla@users.noreply.github.com>
2023-04-23 19:57:09 +0530

    linear gradient changes

--- components/Profile/Bio.jsx
+++ components/Profile/Bio.jsx
@@ -3,8 +3,9 @@ import {AiFillCaretRight} from 'react-icons/ai'
 const Bio = ({section,setSection}) => {
   return (
     <>
-        <div className="bioDetails  w-[90%] sm:w-[60%] h-[30vh] m-auto mt-5 bg-gradient-to-r from-[#36094e] to-[#280e55] rounded-lg text-[#fff]">
-            <textarea className="textarea textarea-lg w-full h-[30vh] m-auto mt-5 bg-gradient-to-r from-[#36094e] to-[#280e55] rounded-lg text-[#fff]" placeholder="Add Bio">
+        <div className="bioDetails  w-[90%] sm:w-[60%] h-[30vh] m-auto mt-5  rounded-lg text-[#fff]">
+            <textarea className="textarea textarea-lg w-full h-[30vh] m-auto mt-5  rounded-lg text-[#fff]" placeholder="Add Bio" 
+            style={{background:"rgba(133, 0, 72, 0.16)"}}>
             </textarea>
             <h3 className='text-[#fff] absolute sm:hidden left-[70%] xs:left-[80%] top-[80%]'
              onClick={()=>{setSection("Skills")}}>
