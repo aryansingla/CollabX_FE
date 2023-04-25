86fb192198da1c33d001026e84c5c58907341b01
simrandhankar <simran.dhankar@itmtb.com>
2023-04-25 19:58:35 +0530

    profile top margin

--- components/Profile/Skills.jsx
+++ components/Profile/Skills.jsx
@@ -60,10 +60,10 @@ const Skills = () => {
   return (
     <div>
           <>
-         <div className="skillDetails w-[90%] sm:w-[60%] h-[40vh] m-auto mt-2 pt-2 sm:pt-2 bg-gradient-to-r from-[#36094e] to-[#280e55] rounded-lg ">
+         <div className="skillDetails w-[90%] sm:w-[60%] h-[40vh] m-auto mt-5 pt-2 sm:pt-2 bg-gradient-to-r from-[#36094e] to-[#280e55] rounded-lg ">
            <div className="justify-between upperPart ">
-            <div className='flex w-[100%] p-2'>
-             <div className='w-[50%] p-4 text-red-500'>
+            <div className='flex sm:flex-row flex-col w-[100%] p-2'>
+             <div className='sm:w-[50%] w-[80%] -ml-4 sm:ml-0 p-4 text-red-500'>
              <select onChange={handleSelectOption} className='w-[70%] p-1 rounded text-black font-semibold hover:cursor-pointer '>
         <option value="">Skills</option>
         {customOption && (
@@ -75,7 +75,7 @@ const Skills = () => {
       
       </select>
              </div>
-           <div className='w-[50%] flex items-center justify-center mr-4 '>  <input type="text" className='w-full p-1 rounded placeholder:text-center placeholder:text-sm placeholder:text-gray-500 placeholder:font-semibold' value={customOption} onChange={handleCustomOptionChange} placeholder='Search and see in dropdown if not in options' /></div>
+           <div className='sm:w-[50%] w-[90%] flex items-center justify-center sm:mb-0 mb-4  mr-4 '>  <input type="text" className='w-full p-1 rounded placeholder:text-center placeholder:text-sm placeholder:text-gray-500 placeholder:font-semibold' value={customOption} onChange={handleCustomOptionChange} placeholder='Search and see in dropdown if not in options' /></div>
            </div>
              <div >
              {selectedOptions.map(option => (
