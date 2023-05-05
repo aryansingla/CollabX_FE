81af7d2bb09597947a85c8fa41ae39117500b603
simrandhankar <simran.dhankar@itmtb.com>
2023-05-05 16:01:05 +0530

    bg-change

---
+++ pages/yourIdeas.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const yourIdeas = () => {
+  return (
+    <div
+    className=' w-full h-screen bg-gradient-to-r from-black to-purple-300'>yourIdeas</div>
+  )
+}
+
+export default yourIdeas
